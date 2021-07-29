e46546eb62624a5526273819f1d8fe87ef816582
hamed6 <h.ghodsinezhad@gmail.com>
2021-07-29 14:57:07 +0300

    n7

--- natasS.bash
+++ natasS.bash
@@ -4,3 +4,6 @@ natas1 -> gtVrDuiDfck831PqWsLEZy5gyDz1clto
 natas2 -> ZluruAthQk7Q2MqmDeTiUij2ZvWy2mBi
 natas3 -> sJIJNW6ucpu6HPZ1ZAchaDtwd7oGrD14
 natas4 ->robots.txt --> Z9tkRkWmpt9Qr7XrR5jWRkgOU901swEZ
+natas5 -> referal link from n5 --> iX6IOfmpN7AYOQGPwtn3fXpbaJVJcHfq
+natas6 -> cookies --> aGoY4q2Dc6MgDq4oL4YtoKtyAg9PeHa1
+natas7 -> includes/secret.inc --> 7z3hEENjQtflzgnT29q7wAvMNfZdh0i9
